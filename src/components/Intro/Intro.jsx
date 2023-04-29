117a9ac3be00b9fa87f28c29ce1852721a174275
httpcyberdata <shieldedsafety@gmail.com>
2023-04-29 11:18:39 -0400

    chore: created title animation by hiding overflow

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,8 +3,29 @@ import './Intro.css'
 const Intro = () => {
   return (
     <div className="i">
-        <div className="i-left">left</div>
-        <div className="i-right">right</div>
+        <div className="i-left">
+            <div className="i-left-wrapper">
+                <h2 className="i-intro">Hello, My name is </h2>
+                <h2 className="i-name">John Burton</h2>
+                <div className="i-title">
+                    <div className="i-title-wrapper">
+                        <div className="i-title-item">Web Developer</div>
+                        <div className="i-title-item">UI/UX Designer</div>
+                        <div className="i-title-item">Photographer</div>
+                        <div className="i-title-item">Writer</div>
+                        <div className="i-title-item">Content Creator</div>
+                    </div>
+                </div>
+                <div className="i-desc">
+                    I design and develop services for customers of all sizes,<br/>
+                     specializing in creating stylish, modern websites,<br/>
+                      web services and online stores.
+                </div>
+            </div>
+        </div>
+        <div className="i-right">
+            
+        </div>
     </div>
   )
 }
